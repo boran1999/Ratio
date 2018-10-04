795801f6ef3d2b30d55906d683ad67bdd654df8e
Anton Borisov <32795987+boran1999@users.noreply.github.com>
2018-10-05 02:10:11 +0800

    Create Rational.h

---
+++ Rational.h
@@ -0,0 +1,35 @@
+#ifndef _RATIONAL_H
+#define _RATIONAL_H
+#include <iostream>
+
+class Rational
+{
+	int numer, denom;
+	Rational();
+	Rational(int number);
+	Rational(int n, int d);
+
+	Rational& operator +=(const Rational& r);
+
+	void simplify();
+
+	Rational operator +(const Rational& r) const;
+
+	Rational operator -() const;
+
+	Rational& operator -(const Rational& r);
+
+	Rational& operator ++();
+	Rational operator ++(int);
+
+	bool operator ==(const Rational& r) const;
+	bool operator !=(const Rational& r) const;
+
+	operator int() const;
+	operator double() const;
+public:
+	friend istream& operator>>(istream& in, Rational& r);
+	friend ostream& operator<<(ostream& out, const Rational& r);
+};
+
+#endif // !_RATIONAL_H
